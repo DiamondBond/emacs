0df06eb6408b3af36306d06c2bdb3cbbc935e58b
Diamond <diamondbond1@gmail.com>
2020-08-28 06:41:19 +0530

    from x220

--- init.el
+++ init.el
@@ -31,11 +31,11 @@
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
 ;; Load theme
-;;(load-theme 'modus-operandi t)
-(load-theme 'spacemacs-light t)
+(load-theme 'modus-operandi t)
+;;(load-theme 'spacemacs-light t)
 
 ;; Start emacs server
-(server-start)
+;;(server-start)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -43,11 +43,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(pdf-tools spacemacs-theme plan9-theme modus-vivendi-theme modus-operandi-theme meghanada company-irony company-c-headers yasnippet-snippets yasnippet company magit treemacs-icons-dired treemacs-evil treemacs async ido-vertical-mode switch-window avy beacon evil swiper which-key dashboard spaceline diminish auto-package-update htmlize use-package)))
+   '(fireplace pdf-tools spacemacs-theme plan9-theme modus-vivendi-theme modus-operandi-theme meghanada company-irony company-c-headers yasnippet-snippets yasnippet company magit treemacs-icons-dired treemacs-evil treemacs async ido-vertical-mode switch-window avy beacon evil swiper which-key dashboard spaceline diminish auto-package-update htmlize use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;;)
+;; )
 '(default ((t (:family "Monaco")))))

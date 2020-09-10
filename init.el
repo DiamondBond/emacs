9d187c706d1a180a917b3ad16522a0b8211b4e7c
Diamond Bond <diamondbond1@gmail.com>
2020-09-10 12:51:47 +0530

    portable

--- init.el
+++ init.el
@@ -31,7 +31,8 @@
 ;;(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 (org-babel-load-file (expand-file-name "~/Dropbox/emacs/config.org"))
 
-(load-theme 'spacemacs-light t)
+;;(load-theme 'spacemacs-light t)
+;;(load-theme 'acme t)
 ;; Disable colored bg for terminal
 (defun on-frame-open (&optional frame)
   "If the frame was created in terminal don't load background color."
@@ -54,10 +55,11 @@
 	 ("\\.x?html?\\'" . default)
 	 ("\\.pdf\\'" . "zathura %s")))
  '(package-selected-packages
-   '(nyan-mode org-pdftools synosaurus xah-math-input ivy flycheck swiper-helm writeroom-mode restart-emacs fireplace pdf-tools spacemacs-theme plan9-theme modus-vivendi-theme modus-operandi-theme meghanada company-irony company-c-headers yasnippet-snippets yasnippet company magit treemacs-icons-dired treemacs-evil treemacs async ido-vertical-mode switch-window avy beacon evil swiper which-key dashboard spaceline diminish auto-package-update htmlize use-package)))
+   '(acme-theme nyan-mode org-pdftools synosaurus xah-math-input ivy flycheck swiper-helm writeroom-mode restart-emacs fireplace pdf-tools spacemacs-theme plan9-theme modus-vivendi-theme modus-operandi-theme meghanada company-irony company-c-headers yasnippet-snippets yasnippet company magit treemacs-icons-dired treemacs-evil treemacs async ido-vertical-mode switch-window avy beacon evil swiper which-key dashboard spaceline diminish auto-package-update htmlize use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Monaco")))))
+ )
+;; '(default ((t (:family "Monaco")))))

04a520a443bf39476c10b6124bd3623cfecde816
Diamond Bond <diamondbond1@gmail.com>
2020-09-17 00:49:14 +0530

    ensure portability init.el is compatible with most setups now. - ensure package install is enabled by default - block commented out simple org-roam config in config.org (not ready for use just yet) - early-init.el minor cleanup

--- early-init.el
+++ early-init.el
@@ -6,12 +6,6 @@
 ;; Defer garbage collection further back in the startup process
 (setq gc-cons-threshold most-positive-fixnum)
 
-;; In Emacs 27+, package initialization occurs before `user-init-file' is
-;; loaded, but after `early-init-file'. Doom handles package initialization, so
-;; we must prevent Emacs from doing it early!
-;;(setq package-enable-at-startup nil)
-;;(advice-add #'package--ensure-init-file :override #'ignore)
-
 ;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
--- init.el
+++ init.el
@@ -13,6 +13,7 @@
  
 (add-hook 'emacs-startup-hook 'startup/revert-file-name-handler-alist)
 (add-hook 'emacs-startup-hook 'startup/reset-gc)
+;;
 
 ;; Initialize melpa repo
 (require 'package)
@@ -25,12 +26,16 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
 
 ;; Load config
-(org-babel-load-file (expand-file-name "~/Dropbox/emacs/config.org"))
+;;(org-babel-load-file (expand-file-name "~/Dropbox/emacs/config.org"))
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
 ;; Load theme
-(load-theme 'spacemacs-light t)
+;;(load-theme 'spacemacs-light t)
+(load-theme 'leuven t)
 
 ;; Start emacs server
 (server-start)
@@ -46,10 +51,11 @@
 	 ("\\.x?html?\\'" . default)
 	 ("\\.pdf\\'" . "zathura %s")))
  '(package-selected-packages
-   '(vigenere ivy-rich rainbow-delimiters lorem-ipsum pdf-view-restore pdf-tools acme-theme nyan-mode synosaurus xah-math-input ivy flycheck swiper-helm writeroom-mode restart-emacs fireplace spacemacs-theme modus-vivendi-theme modus-operandi-theme company-irony company-c-headers yasnippet-snippets yasnippet company magit treemacs-icons-dired treemacs-evil treemacs async ido-vertical-mode switch-window avy beacon evil swiper which-key dashboard spaceline diminish auto-package-update htmlize use-package)))
+   '(deft org-roam-server org-roam vigenere ivy-rich rainbow-delimiters lorem-ipsum pdf-view-restore acme-theme nyan-mode org-pdftools synosaurus xah-math-input ivy flycheck swiper-helm writeroom-mode restart-emacs fireplace pdf-tools spacemacs-theme plan9-theme modus-vivendi-theme modus-operandi-theme company-irony company-c-headers yasnippet-snippets yasnippet company magit treemacs-icons-dired treemacs-evil treemacs async ido-vertical-mode switch-window avy beacon evil swiper which-key dashboard spaceline diminish auto-package-update htmlize use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ ;;'(default ((t (:family "Monaco"))))
+)

b92cc725ba74e214e2b27e5108ffe78e1e3c4ee5
Diamond Bond <diamondbond1@gmail.com>
2020-12-31 00:33:25 +0530

    latex

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -137,7 +137,8 @@
        (javascript +lsp)        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       latex             ; writing papers in Emacs has never been so fun
+       ;;latex             ; writing papers in Emacs has never been so fun
+       (latex +latexmk +cdlatex +lsp)
        ;;lean
        ;;factor
        ;;ledger            ; an accounting system in Emacs

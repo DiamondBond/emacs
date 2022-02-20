5e5619b705fd8e040b1078344ba8412d383c4d84
Diamond Bond <diamond.nigel75@Gmail.com>
2022-02-21 02:53:07 +0530

    tame gc we set gc initially to most-positive-fixnum then we reset.

--- early-init.el
+++ early-init.el
@@ -3,16 +3,40 @@
 ;; Emacs HEAD (27+) introduces early-init.el, which is run before init.el,
 ;; before package and UI initialization happens.
 
+;; A big contributor to startup times is garbage collection. We up the gc
+;; threshold to temporarily prevent it from running, then reset it later by
+;; enabling `gcmh-mode'. Not resetting it will cause stuttering/freezes.
+(setq gc-cons-percentage-original gc-cons-percentage
+	  gc-cons-threshold-original gc-cons-threshold)
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; Prevent unwanted runtime compilation for gccemacs (native-comp) users;
+;; packages are compiled ahead-of-time when they are installed and site files
+;; are compiled when gccemacs is installed.
+(setq native-comp-deferred-compilation nil)
+
 ;; In Emacs 27+, package initialization occurs before `user-init-file' is
 ;; loaded, but after `early-init-file'.
 (setq package-enable-at-startup nil)
 
-;; Defer garbage collection further back in the startup process
-(setq gc-cons-threshold most-positive-fixnum)
+;; In noninteractive sessions, prioritize non-byte-compiled source files to
+;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
+;; to skip the mtime checks on every *.elc file.
+(setq load-prefer-newer noninteractive)
+
+;; Premature redisplays can substantially affect startup times and produce
+;; ugly flashes of unstyled Emacs.
+(setq-default inhibit-redisplay t
+			  inhibit-message t)
+(add-hook 'window-setup-hook
+		  (lambda ()
+			(setq-default inhibit-redisplay nil
+						  inhibit-message nil)
+			(redisplay)))
 
 ;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
 (setq tool-bar-mode nil
-  menu-bar-mode nil)
+	  menu-bar-mode nil)
 (when (fboundp 'set-scroll-bar-mode)
   (set-scroll-bar-mode nil))
 
--- init.el
+++ init.el
@@ -1,18 +1,10 @@
 ;; Make emacs startup faster
-(setq gc-cons-threshold 402653184
-	  gc-cons-percentage 0.6)
-
 (defvar startup/file-name-handler-alist file-name-handler-alist)
 (setq file-name-handler-alist nil)
 (defun startup/revert-file-name-handler-alist ()
   (setq file-name-handler-alist startup/file-name-handler-alist))
 
-(defun startup/reset-gc ()
-  (setq gc-cons-threshold 16777216
-		gc-cons-percentage 0.1))
-
 (add-hook 'emacs-startup-hook 'startup/revert-file-name-handler-alist)
-(add-hook 'emacs-startup-hook 'startup/reset-gc)
 
 ;; Initialize melpa repo
 (require 'package)

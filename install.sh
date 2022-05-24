d0c50b52e782d78cd35ef61d25f4e1031cb6e4ed
Diamond Bond <diamond.nigel75@Gmail.com>
2022-05-25 04:50:46 +0530

    Cleanup

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-mkdir -p ~/.emacs.d/; cp init.el ~/.emacs.d/; cp early-init.el ~/.emacs.d/; cp config.org ~/.emacs.d/; cp -r img ~/.emacs.d/; cp -r snippets ~/.emacs.d/; cp -r modules ~/.emacs.d/; cp userconfig.org ~/.emacs.d/
+# Make emacs.d
+mkdir -p ~/.emacs.d/;
+
+# Install
+cp init.el ~/.emacs.d/; cp early-init.el ~/.emacs.d/; cp config.org ~/.emacs.d/; cp userconfig.org ~/.emacs.d/
+
+# Copy libs
+cp -r img ~/.emacs.d/; cp -r snippets ~/.emacs.d/; cp -r modules ~/.emacs.d/; cp -r resources ~/.emacs.d/;

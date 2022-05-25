3565d09f2ef0030dcbc22423961984b714a9b0c5
Diamond Bond <diamond.nigel75@Gmail.com>
2022-05-25 16:05:54 +0530

    update yasnippets

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ mkdir -p ~/.emacs.d/;
 cp init.el ~/.emacs.d/; cp early-init.el ~/.emacs.d/; cp config.org ~/.emacs.d/; cp userconfig.org ~/.emacs.d/
 
 # Copy libs
-cp -r modules ~/.emacs.d/; cp -r resources ~/.emacs.d/; cp -r img ~/.emacs.d/;
+cp -r modules ~/.emacs.d/; cp -r resources ~/.emacs.d/; cp -r snippets ~/.emacs.d/; cp -r img ~/.emacs.d/;

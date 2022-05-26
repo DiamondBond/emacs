d91cea29cb357f6550b47945a7b1b4b14cc47315
Diamond Bond <diamond.nigel75@Gmail.com>
2022-05-26 08:43:46 +0530

    cleanup
    Signed-off-by: Diamond Bond <diamond.nigel75@Gmail.com>

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Make emacs.d
-mkdir -p ~/.emacs.d/;
-
-# Install
-cp init.el ~/.emacs.d/; cp early-init.el ~/.emacs.d/; cp config.org ~/.emacs.d/; cp userconfig.org ~/.emacs.d/
-
-# Copy libs
-cp -r modules ~/.emacs.d/; cp -r resources ~/.emacs.d/; cp -r snippets ~/.emacs.d/; cp -r img ~/.emacs.d/;

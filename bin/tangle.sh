084e588e2a1939cfcce82ef0d6dbcac70efc5541
Diamond Bond <diamond.nigel75@Gmail.com>
2022-05-28 01:27:04 +0530

    Sync tangle configuration with doom conventions
    Use ~/bin as some users might already have this added to their $PATH.

--- bin/tangle.sh
+++ bin/tangle.sh
@@ -5,6 +5,9 @@ function org-tangle () {
 	emacs --batch -l org "$1" -f org-babel-tangle
 }
 
+# set cwd
+cd ~/.emacs.d/
+
 # tangle base configuration
 org-tangle config.org
 org-tangle globals.org
@@ -15,3 +18,6 @@ org-tangle modules/evil.org
 org-tangle modules/reddit.org
 org-tangle modules/discord.org
 org-tangle modules/mail.org
+
+# exit
+exit 0

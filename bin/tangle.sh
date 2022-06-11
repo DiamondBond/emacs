de95e4d266e05491bd8fd02e02e77966e1bf7c7e
Diamond Bond <diamond.nigel75@Gmail.com>
2022-06-11 23:18:48 +0530

    Update tangle.sh

--- bin/tangle.sh
+++ bin/tangle.sh
@@ -18,9 +18,6 @@ org-tangle modules/evil.org
 org-tangle modules/mail.org
 org-tangle modules/reddit.org
 org-tangle modules/discord.org
-#org-tangle modules/honeydew.org
-org-tangle modules/archive/laptop/laptop.org
-org-tangle modules/archive/desktop/desktop.org
 
 if [ $# -eq 1 ]
   then

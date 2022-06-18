3c90cd3cc21894846518109d05ca1b808ff739f2
Diamond Bond <diamond.nigel75@Gmail.com>
2022-06-19 01:55:16 +0530

    testing ui changes

---
+++ custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ ;; '(display-battery-mode t)
+ ;; '(display-time-mode t)
+ '(org-file-apps
+   '((auto-mode . emacs)
+	 ("\\.mm\\'" . default)
+	 ("\\.x?html?\\'" . default)
+	 ("\\.pdf\\'" . emacs)))
+ '(warning-suppress-log-types '((use-package) (browse-url) (comp)))
+ '(warning-suppress-types '((use-package) (browse-url) (comp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

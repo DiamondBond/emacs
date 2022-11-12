6040740d1430ffe2d050bdede381a6759c535342
Diamond Bond <diamond.nigel75@Gmail.com>
2022-11-12 14:40:11 +0530

    Update settings

--- custom.el
+++ custom.el
@@ -3,8 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;; '(display-battery-mode t)
- ;; '(display-time-mode t)
  '(org-file-apps
    '((auto-mode . emacs)
 	 ("\\.mm\\'" . default)

17c4365902c532fb3db6ed0099ede0eaaa2f7acc
Diamond Bond <diamondbond1@gmail.com>
2023-01-17 21:05:12 +0530

    update custom.el

--- custom.el
+++ custom.el
@@ -16,20 +16,4 @@
 	 ((flymake flymake))
 	 (use-package)
 	 (browse-url)
-	 (comp)))
- '(warning-suppress-types
-   '(((flymake flymake))
-	 ((flymake flymake))
-	 ((flymake flymake))
-	 ((flymake flymake))
-	 (use-package)
-	 (browse-url)
 	 (comp))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(minimap-active-region-background ((((background dark)) (:background "#545454545454")) (t (:background "#cececececece"))) nil :group)
- ;;'(scroll-bar ((t (:background "gray30"))))
- )

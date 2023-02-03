a91188c59d9f29d75d9b812bf7cbacddc0bd6930
Diamond Bond <diamondbond1@gmail.com>
2023-02-03 08:05:54 +0530

    mu4e-modeline-mode -> disable only on certain system

--- init.el
+++ init.el
@@ -3268,9 +3268,13 @@ If the prefix argument ARG is non-nil, convert the text to uppercase."
 
   ;; disable mu4e-modeline
   ;;(mu4e-modeline-mode -1)
-  ;; (add-hook 'mu4e-main-mode-hook (lambda () (mu4e-modeline-mode -1)))
-  ;;(add-hook 'dashboard-mode-hook (lambda () (mu4e-modeline-mode -1)))
-  ;; (add-hook 'emacs-startup-hook (lambda () (mu4e-modeline-mode -1)))
+
+  ;; disable mu4e modeline on system with newer mu4e package
+  (when (string= (system-name) "nitro")
+	(progn
+	  (add-hook 'mu4e-main-mode-hook (lambda () (mu4e-modeline-mode -1)))
+	  (add-hook 'dashboard-mode-hook (lambda () (mu4e-modeline-mode -1)))
+	  (add-hook 'emacs-startup-hook (lambda () (mu4e-modeline-mode -1)))))
 
   ;; start mu4e
   (mu4e t))

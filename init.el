03bc1bfb5d3d8bd1de2f78493f31f4ffca8b0684
Diamond Bond <diamondbond1@gmail.com>
2023-02-10 11:34:23 +0530

    add GOAL

--- init.el
+++ init.el
@@ -2534,6 +2534,34 @@ Useful for prompts such as `eval-expression' and `shell-command'."
 (use-package sicp
   :straight t)
 
+;; GOAL
+(define-derived-mode goal-mode lisp-mode
+  "GOAL")
+;; make gc files use our derived mode
+(add-to-list 'auto-mode-alist '("\\.gc?\\'" . goal-mode))
+;; run setup-goal when we enter lisp mode
+(add-hook 'goal-mode-hook 'setup-goal)
+(defun setup-goal ()
+  ;; if we are in a gc file, change indent settings for GOAL
+  (when (and (stringp buffer-file-name)
+             (string-match "\\.gc\\'" buffer-file-name))
+    (put 'with-pp      'common-lisp-indent-function 0)
+    (put 'while        'common-lisp-indent-function 1)
+    (put 'rlet         'common-lisp-indent-function 1)
+    (put 'until        'common-lisp-indent-function 1)
+    (put 'countdown    'common-lisp-indent-function 1)
+    (put 'defun-debug  'common-lisp-indent-function 2)
+    (put 'defenum      'common-lisp-indent-function 2)
+
+    ;; disable slime
+    (slime-mode -1)
+
+    ;; indent for common lisp, this makes if's look nicer
+    (custom-set-variables '(lisp-indent-function 'common-lisp-indent-function))
+    (autoload 'common-lisp-indent-function "cl-indent" "Common Lisp indent.")
+    ;; use spaces, not tabs
+    (setq-default indent-tabs-mode nil)))
+
 ;; JSON
 (use-package json-mode
   :straight t

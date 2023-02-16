6ff822551faa4facd089196ca313a9a6390c52d8
Diamond Bond <diamondbond1@gmail.com>
2023-02-16 12:09:59 +0530

    add exec-path-from-shell cfg

--- lisp/packages.el
+++ lisp/packages.el
@@ -15,7 +15,10 @@
 (use-package exec-path-from-shell
   :straight t
   :config
-  (exec-path-from-shell-initialize))
+  (when (memq window-system '(mac ns x))
+	(exec-path-from-shell-initialize))
+  (when (daemonp)
+	(exec-path-from-shell-initialize)))
 
 ;; Async
 (use-package async
